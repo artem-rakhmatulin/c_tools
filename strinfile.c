3be4170aa77cf0c3f5b7e140597b8f7e34045c5a
Artem Rakhmatulin <rakhmatulin@gmail.com>
2021-02-10 23:10:05 +0100

    strinfile.c: exception handling.

--- strinfile.c
+++ strinfile.c
@@ -12,18 +12,31 @@ int main(int argc, char **argv)
 {
   FILE *fp = NULL;
   char str[MAX_STR_LENGTH] = {'\0'};
+  int cnt = 0;
 
   if (argc != 3)
+  {
+    fprintf(stderr, "USAGE: %s filename string\n", argv[0]);
     exit(EXIT_FAILURE);
+  }
 
   fp = fopen(argv[1], "r");
 
   if (NULL == fp)
+  {
+    fprintf(stderr, "FILE OPEN ERROR: %s\n", argv[1]);
     exit(EXIT_FAILURE);
+  }
 
   while (fgets(str, MAX_STR_LENGTH, fp))
     if (strstr(str, argv[2]))
+    {
       fputs(str, stdout);
+      cnt++;
+    }
+
+  if (!cnt)
+    printf("String \"%s\" not found.\n", argv[2]);
 
   fclose(fp);
 
